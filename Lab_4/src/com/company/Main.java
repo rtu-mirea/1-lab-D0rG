60f34eb3ffadcfd06dd983526f43b6099457ad6d
D0rG <55016092+D0rG@users.noreply.github.com>
2019-11-30 09:40:13 +0300

    interface

--- Lab_4/src/com/company/Main.java
+++ Lab_4/src/com/company/Main.java
@@ -7,21 +7,72 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String Buf = in.nextLine();
-	    //Car car = new Car("", Buf, "", "", "", "", Buf);
-        Car car = new Car("Tesla cybertrack", "е404рр777", "Белоснежно-чёрный", "Илон", "Маск", "Калифорния", "23.04.2018");
 
-        FileManager F = new FileManager("Test.txt");
-        //System.out.println(F.GetCarString(car));
+        String StringBuf = "";
+        System.out.print("Введите название файла: ");
+        StringBuf = in.nextLine();
+        StringBuf.trim();
+
+        System.out.print("Введите кол-во автомобилей которые хотите занести в базу: ");
+        int IntBuf = in.nextInt();
+
+        //FileManager F = new FileManager("Test.txt");
+        FileManager F = new FileManager(StringBuf);
+
+        if(F.FileExist()){
+            System.out.println("Файл существует");
+        }else{
+            System.out.println("Файл не существует и будет создан.");
+            try {
+            F.CreateFile();
+            }
+            catch (Exception e){
+                System.err.println(e);
+            }
+
+        }
+
+        for(int i = 0; i < IntBuf; ++i){
+            System.out.println("Вы вводите автомобиль №" + (i + 1));
+            System.out.print("Введите модель автомобиля: ");
+            String StringBuf1 = in.nextLine();
+            System.out.print("Введите номер автомобиля: ");
+            String StringBuf2 = in.nextLine();
+            System.out.print("Введите цвет автомобиля: ");
+            String StringBuf3 = in.nextLine();
+            System.out.print("Введите Имя владельца: ");
+            String StringBuf4 = in.nextLine();
+            System.out.print("Введите Фамилию владельца: ");
+            String StringBuf5 = in.nextLine();
+            System.out.print("Введите аддрес: ");
+            String StringBuf6 = in.nextLine();
+            System.out.print("Введите дату пследнего тех.осмотра (xx.xx.xxxx): ");
+            String StringBuf7 = in.nextLine();
+
+            Car car = new Car(StringBuf1,StringBuf2,StringBuf3,StringBuf4,StringBuf5,StringBuf6,StringBuf7);
+            try {
+                //F.CreateFile();
+                F.WriteInFile(car);
+                ArrayList<Car> Cars = F.GetCars();
+            }
+            catch (IOException e){
+                System.err.println("Ошибка: " + e);
+            }
+        }
+
+        //Car car = new Car("Tesla cybertrack", "е404рр777", "Белоснежно-чёрный", "Илон", "Маск", "Калифорния", "23.04.2018");
+        //System.out.println(F.FindOwner("e404pp777"));
+
         try {
-            //F.CreateFile();
-            F.WriteInFile(car);
+            System.out.println("Введите номер автомобиля что бы найти владельца: ");
+            StringBuf = in.nextLine();
+            System.out.println(F.FindOwner(StringBuf));
             ArrayList<Car> Cars = F.GetCars();
-            System.out.println(F.FindOwner("e404pp777"));
-            //System.out.println(F.ReadInFile());
+
         }
         catch (IOException e){
-            System.out.println("Ошибка: " + e);
+            System.err.println("Ошибка: " + e);
         }
+
     }
 }
